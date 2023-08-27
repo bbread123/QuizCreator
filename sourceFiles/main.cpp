2b31083b59b3788149be231b79aa77c18c05e177
Samir Kharal <samirkharal223@gmail.com>
2023-08-27 20:42:06 +0545

    Update main.cpp

--- sourceFiles/main.cpp
+++ sourceFiles/main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 //    loading stylesheet
-//    QFile styleSheetFile("C:/Users/samir/Documents/QTproj/test/Darkeum.qss");
+//    QFile styleSheetFile("C:/Users/username/Documents/QTproj/test/Darkeum.qss");
     QFile styleSheetFile("./Darkeum.qss");
     styleSheetFile.open(QFile::ReadOnly);
     QString styleSheet = QLatin1String(styleSheetFile.readAll());
